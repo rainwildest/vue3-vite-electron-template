9fa85f4051fcb9b611646c071980dff4cc9c16b3
rainwildest <rainwildest@163.com>
2021-08-02 16:34:35 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import path from "path";
 
 export default defineConfig((env) => {
   const { mode } = env;
